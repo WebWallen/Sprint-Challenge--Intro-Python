fff352b4edf44c7a3b778c1a4bb56bf6382fdd29
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-01-17 12:52:56 -0500

    Completed OOP challenges. Also added/deleted/edited comments for clarity. 3/3 done.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -7,32 +7,20 @@ class City:
     self.lat = lat
     self.lng = lng
   
-  # Remember: this should not be indented (and if it is, the method won't register whatsoever!!!)
+  # Remember: this should NOT be indented (and if it is, the method won't register whatsoever!)
   def __repr__(self):
     return f"{self.name}, {self.lat}, {self.lng}"
 
-# We have a collection of US cities with population over 750,000 stored in the
-# file "cities.csv". (CSV stands for "comma-separated values".)
-#
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
-# to read this file so that each record is imported into a City instance. Then
-# return the list with all the City instances from the function.
-# Google "python 3 csv" for references and use your Google-fu for other examples.
-#
-# Store the instances in the "cities" list, below.
-#
-# Note that the first line of the CSV is header that describes the fields--this
-# should not be loaded into a City object.
 import csv
+# CSV stands for "Comma Separated Values"
 
 cities = []
+# Always begin by initializing an empty array when you plan to create new data based on existing file structure
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
 
     file = open('src/cityreader/cities.csv', 'r')
+    # Open the linked file (note: often have to include /more-paths/than-expected) in 'read-only' mode
     with file:
       # DictReader converts file into format Python can manipulate for programming purposes
       csv_reader = csv.DictReader(file)
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -1,32 +1,24 @@
-# To the GroundVehicle class, add method drive() that returns "vroooom".
-#
-# Also change it so the num_wheels defaults to 4 if not specified when the
-# object is constructed.
+# Illustrate How to Create Class Objects with Default Behaviors You Can Override
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
+    # Can set a default value inside the init statementand override for exceptions
         self.num_wheels = num_wheels
+    def drive(self):
+    # Don't forget you always have to pass "self" in as an argument to Python class methods
+        return "vroooom"
 
-    # TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        self.num_wheels = num_wheels 
+    def drive(self):
+        return "BRAAAP!!"
 
+harley = Motorcycle()
+chrysler = GroundVehicle()
 
-# Subclass Motorcycle from GroundVehicle.
-#
-# Make it so when you instantiate a Motorcycle, it automatically sets the number
-# of wheels to 2 by passing that to the constructor of its superclass.
-#
-# Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+print(harley.drive())
+print(chrysler.drive())
 
-# TODO
-
-vehicles = [
-    GroundVehicle(),
-    GroundVehicle(),
-    Motorcycle(),
-    GroundVehicle(),
-    Motorcycle(),
-]
-
-# Go through the vehicles list and print the result of calling drive() on each.
-
-# TODO
+print(harley.num_wheels)
+print(chrysler.num_wheels)
